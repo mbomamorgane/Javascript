b40be6e93ebb67045bb7ea9e764a747b308bbba9
Morgane Mboma <54062684+mbomamorgane@users.noreply.github.com>
2019-09-19 16:09:48 +0200

    base4

---
+++ js-exercises-base4/exo1/1.js
@@ -0,0 +1,8 @@
+function over() {
+    document.images[0].src="./image/image1_2.jpg";
+    this.style.border = '3px solide rouge'
+}
+
+function out() {
+    document.images[0].src='./image/image1.jpg';
+}
